676686cad1557225ddfbd7e697e8b27fb0495642
shiam2018 <38714715+shiam2018@users.noreply.github.com>
2019-12-14 02:23:39 +0200

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "4aab698cc9e01dab5546f8b27879847b";
 $scopes = "read_orders,write_products";
-$redirect_uri = "https://myhive.top/generate_token.php";
+$redirect_uri = "http://mynewacc.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
