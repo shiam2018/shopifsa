08f16cc791afde439eb63d35ed6b43038345785d
shiam2018 <38714715+shiam2018@users.noreply.github.com>
2019-12-14 02:17:14 +0200

    Update install.php

--- install.php
+++ install.php
@@ -2,9 +2,9 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "4aab698cc9e01dab5546f8b27879847b";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://myhive.top/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
